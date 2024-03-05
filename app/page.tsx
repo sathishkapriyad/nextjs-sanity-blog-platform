2fc3cc4db007741a58d77eec8772e143faf1a97b
spriyad <spriyad@SathishKas-MBP.home>
2024-03-05 17:10:27 +0000

    Add load more functionality to blog list page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import Image from "next/image";
 import { client, urlFor } from "./lib/sanity";
 import { simpleBlogCard, advertType } from "./lib/interface";
@@ -7,13 +9,14 @@ import Link from "next/link";
 import { badgeVariants } from "@/components/ui/badge"
 import { ArrowBigRight, ArrowRight, Space } from "lucide-react";
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel";
+import { useEffect, useState } from "react";
 
 export const revalidate = 30; // revalidate at most 30 seconds
 
-async function getData() {
+async function getData(start = 0, limit = 6) {
     try {
         const query = `
-      *[_type == 'blog'] | order(_createdAt desc) {
+      *[_type == 'blog'] | order(_createdAt desc)[$start...$end] {
         title,
         smallDescription,
         "currentSlug": slug.current,
@@ -25,7 +28,11 @@ async function getData() {
         categories[]->{title},
         featured,
       }`;
-        const data = await client.fetch(query);
+        const params = {
+            start: start,
+            end: start + limit,
+        };
+        const data = await client.fetch(query, params);
         return data;
     } catch (error) {
         console.error("Failed to fetch data:", error);
@@ -51,15 +58,56 @@ async function getAdvert() {
 
 
 
-export default async function Home() {
-    const data: simpleBlogCard[] = await getData();
-    const advertdata: advertType[] = await getAdvert();
+export default  function Home() {
+    // const data: simpleBlogCard[] = await getData();
+    // const advertdata: advertType[] = await getAdvert();
 
-    const headerAdverts = advertdata.filter(ad => ad.adposition === 'header');
+    // const headerAdverts = advertdata.filter(ad => ad.adposition === 'header');
     // const sidebarAdverts = advertdata.filter(ad => ad.adposition === 'sidebar');
     // const footerAdverts = advertdata.filter(ad => ad.adposition === 'footer');
 
-    const featuredPosts = data.filter(post => post.featured);
+    const [posts, setPosts] = useState<simpleBlogCard[]>([]);
+    const [start, setStart] = useState(0); // Starting index for posts
+    const [loading, setLoading] = useState(false); // Loading state to control button behavior
+    const [hasMore, setHasMore] = useState(true); // Flag to check if more posts are available to load
+
+
+    // Initial posts loading
+    useEffect(() => {
+        const loadInitialPosts = async () => {
+            setLoading(true);
+            try {
+                const initialPosts = await getData(0, 6); // Fetch first 5 posts
+                setPosts(initialPosts); // Update the posts state
+                setHasMore(initialPosts.length === 6);
+            } catch (error) {
+                console.error("Failed to load initial posts:", error);
+                // Handle error appropriately
+            }
+            setLoading(false);
+        };
+
+        loadInitialPosts();
+    }, []);
+
+    const loadMore = async () => {
+        if (!loading && hasMore) {
+            setLoading(true);
+            try {
+                const newStart = start + 6; // Increment start index
+                const additionalPosts = await getData(newStart, 6); // Fetch next set of posts
+                setPosts(prevPosts => [...prevPosts, ...additionalPosts]); // Append new posts
+                setStart(newStart);
+                setHasMore(additionalPosts.length === 5);
+            } catch (error) {
+                console.error("Failed to load more posts:", error);
+                // Handle error appropriately
+            }
+            setLoading(false);
+        }
+    };
+
+    const featuredPosts = posts.filter(post => post.featured);
 
     return (
         <>
@@ -109,12 +157,12 @@ export default async function Home() {
                 ))}
             </div>
 
-            <div className="w-full">
+            <div className="w-full pl-10 pr-10">
                 <h3 className="text-lg  text-gray-800 mt-8 mb-5 dark:text-gray-300">More topics</h3>
 
                 <Carousel className="w-full ">
                     <CarouselContent className="-ml-1">
-                        {data.map((post, idx) => (
+                        {posts.map((post, idx) => (
                             <CarouselItem key={idx} className="pl-1 md:basis-1/2 lg:basis-1/6">
                                 <div className="p-1">
                                     <Card>
@@ -141,7 +189,7 @@ export default async function Home() {
 
             <div className="grid grid-cols-1 md:grid-cols-3 mt-5 gap-5">
 
-                {data.map((post, idx) => (
+                {posts.map((post, idx) => (
                     <Card key={idx} className="flex flex-col md:flex-row">
                         <Image
                             src={urlFor(post.titleImage).url()}
@@ -175,6 +223,17 @@ export default async function Home() {
                         </CardContent>
                     </Card>
                 ))}
+            </div>
+
+            <div className="w-full flex justify-center mt-10">
+            {hasMore && (
+                <div>
+                    <Button variant="outline" onClick={loadMore} disabled={loading}>
+                    {loading ? 'Loading...' : 'Load more stories'}
+                    </Button>
+
+                </div>
+            )}
             </div>
 
         </>
