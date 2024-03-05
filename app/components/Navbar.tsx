3bc74f36206e4eb792e6803623787b9f2611054e
spriyad <spriyad@SathishKas-MBP.home>
2024-03-05 17:39:20 +0000

    update header menu item

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -15,6 +15,33 @@ export default function Navbar() {
           </span>
         </Link>
       </div>
+      <div className="flex-grow px-4">
+        <div className="">
+      
+        <Link href="/" passHref>
+          <span className="ml-5 text-gray-800 mt-8 mb-5 dark:text-gray-300 text-sm cursor-pointer ">
+            <span className="">Latest Stories</span>
+          </span>
+        </Link>
+        <Link href="/" passHref>
+          <span className="ml-5 text-gray-800 mt-8 mb-5 dark:text-gray-300 text-sm cursor-pointer">
+            <span className="">Product Reviews</span>
+          </span>
+        </Link>
+        <Link href="/" passHref>
+          <span className="ml-5 text-gray-800 mt-8 mb-5 dark:text-gray-300 text-sm cursor-pointer">
+            <span className="">AI Stories</span>
+          </span>
+        </Link>
+
+        <Link href="/" passHref>
+          <span className="ml-5 text-gray-800 mt-8 mb-5 dark:text-gray-300 text-sm cursor-pointer">
+            <span className="">Learn Programming</span>
+          </span>
+        </Link>
+
+        </div>
+      </div>
       <div className="flex-grow px-4 max-w-2xl">
         <div className="relative">
           <MagnifyingGlassIcon className="absolute left-3 top-1/2 transform -translate-y-1/2 h-5 w-5 text-gray-400" />
