698ea86655ddb0bbc73423369f53803bcabf425f
spriyad <spriyad@SathishKas-MBP.home>
2024-03-06 23:07:48 +0000

    added styles to tags page

--- app/tags/[tags]/page.tsx
+++ app/tags/[tags]/page.tsx
@@ -8,6 +8,7 @@ import { badgeVariants } from "@/components/ui/badge";
 import { Card, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/router";
+import { ArrowRightCircle } from "lucide-react";
 
 export const revalidate = 30; // revalidate at most 30 seconds
 
@@ -44,38 +45,43 @@ export default async function BlogsByTag({
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 mt-5 gap-5">
-      {data.length > 0 ? (
-        data.map((blog, idx) => (
-          <Card key={idx}>
-            <Image
-              src={urlFor(blog.titleImage).url()}
-              alt="image"
-              width={500}
-              height={500}
-              className="rounded-t-lg h-[200px] object-cover"
-            />
+     {data.map((post, idx) => (
+                    <Card key={idx} className="flex flex-col md:flex-row transition duration-500 hover:scale-105">
+                        <Image
+                            src={urlFor(post.titleImage).url()}
+                            alt="image"
+                            width={500}
+                            height={500}
+                            className="rounded-t-lg h-[200px] object-contain md:w-1/2"
+                        />
 
-            <CardContent className="mt-5">
-              <h3 className="text-lg line-clamp-2 font-bold">{blog.title}</h3>
-              <p className="line-clamp-3 text-sm mt-2 text-gray-600 dark:text-gray-300">
-                {blog.smallDescription}
-              </p>
-              <Button asChild className="w-full mt-7 py-5">
-                <Link href={`/blog/${blog.currentSlug}`}>Read More</Link>
-              </Button>
-              <div className="mt-4">
-                                {blog.tags.map((tag, tagIdx) => (
-                                   <Link key={tagIdx} href={`/tags/${tag}`} passHref>
-                                   <span className={`${badgeVariants({ variant: "outline" })} mx-2 my-1 cursor-pointer inline-block`}>{tag}</span>
-                               </Link>
-                                ))}
-                            </div>
-            </CardContent>
-          </Card>
-        ))
-      ) : (
-        <p>No posts found for this tag.</p>
-      )}
+                        <CardContent className="mt-5 flex-1 md:w-1/2 px-4 py-2">
+
+                        <div className="card-tags flex items-center"> {/* Container for tags */}
+                        {Array.isArray(post.tags) ? (
+                          post.tags.map((tag, tagIdx) => (
+                            <Link key={tagIdx} href={`/tags/${tag}`} className="mr-2">
+                              <span className={`${badgeVariants({ variant: "outline" })} mx-2 my-1  cursor-pointer inline-block uppercase`}>{tag}</span>
+                            </Link>
+                          ))
+                        ) : post.tags ? (
+                          <Link href={`/tags/${post.tags}`} className="mr-2">
+                           <span className={`${badgeVariants({ variant: "outline" })} mx-2 my-1  cursor-pointer inline-block uppercase`}> {post.tags}</span>
+                          </Link>
+                        ) : null}
+                      </div>
+
+                            <h3 className="text-lg line-clamp-2 ">{post.title}</h3>
+                            <p className="line-clamp-3 text-sm mt-2 text-gray-600 dark:text-gray-300">
+                                {post.smallDescription}
+                            </p>
+                            <Link href={`/blog/${post.currentSlug}`} className="flex items-center text-sm text-blue-500 mt-4 mb-4 text-sm font-semibold">
+                                Read More <ArrowRightCircle className="h-5 w-6 text-blue-500 ml-2" />
+                            </Link>
+                        
+                        </CardContent>
+                    </Card>
+                ))}
     </div>
   );
 }
