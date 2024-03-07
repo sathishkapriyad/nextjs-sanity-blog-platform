3bf300681f10f001300b6275188ef7299e0a464a
spriyad <spriyad@SathishKas-MBP.home>
2024-03-07 00:15:47 +0000

    Update navbar styles

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,5 +1,4 @@
-'use client';
-
+'use client'; 
 import { useState } from 'react';
 import Link from 'next/link';
 import { ModeToggle } from './ModeToggle';
@@ -11,8 +10,9 @@ export default function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
-    <>
-      <nav className="sticky top-0 z-50 w-full flex items-center justify-between px-4 py-5 bg-white shadow-md">
+    <nav className="sticky top-0 z-50 w-full flex items-center justify-between px-4 py-5 bg-white shadow-md">
+      {/* Left Section: Logo & Links */}
+      <div className="flex items-center"> 
         {/* Hamburger icon (for mobile) */}
         <div className="md:hidden">
           <button onClick={() => setIsMenuOpen(!isMenuOpen)}>
@@ -29,26 +29,30 @@ export default function Navbar() {
         </Link>
 
         {/* Desktop Navigation */}
-        <div className="hidden md:flex items-center ml-8">
-          <Link href="/">
-            <span className="ml-5 text-gray-800 dark:text-gray-300 text-sm cursor-pointer">Latest Stories</span>
-          </Link>
-          <Link href="/">
-            <span className="ml-5 text-gray-800 dark:text-gray-300 text-sm cursor-pointer">Product Reviews</span>
-          </Link>
-          <Link href="/">
-            <span className="ml-5 text-gray-800 dark:text-gray-300 text-sm cursor-pointer">AI Stories</span>
-          </Link>
-          <Link href="/">
-            <span className="ml-5 text-gray-800 dark:text-gray-300 text-sm cursor-pointer">Learn Programming</span>
-          </Link>
+        <div className="hidden md:flex items-center ml-8 desktop-navbar">
+        <Link href="/" className="py-3">
+        <span className="py-2 text-gray-800 dark:text-gray-300 cursor-pointer">Latest Stories</span>
+        </Link>
+        <Link href="/" className="py-3">
+        <span className="py-2 text-gray-800 dark:text-gray-300 cursor-pointer">Product Reviews</span>
+        </Link>
+        <Link href="/" className="py-3">
+        <span className="py-2 text-gray-800 dark:text-gray-300 cursor-pointer">AI Stories</span>
+        </Link>
+        <Link href="/" className="py-3">
+        <span className="py-2 text-gray-800 dark:text-gray-300 cursor-pointer">Learn Programming</span>
+        </Link>
+          {/* ... more links */}
+        </div>
+      </div>
 
-          {/* Desktop Search Bar */}
-          <div className="flex-grow ml-8">
-            <div className="relative">
-              <MagnifyingGlassIcon className="absolute left-3 top-1/2 transform -translate-y-1/2 h-5 w-5 text-gray-400" />
-              <Input placeholder="Find your byte..." className="pl-10 pr-4 py-2 w-full" />
-            </div>
+      {/* Right Section: Search Bar & Mode Toggle */}
+      <div className="flex items-center ml-8 space-x-4"> 
+        {/* Desktop Search Bar */}
+        <div className="hidden md:flex">
+          <div className="relative">
+            <MagnifyingGlassIcon className="absolute left-3 top-1/2 transform -translate-y-1/2 h-5 w-5 text-gray-400" />
+            <Input placeholder="Find an article..." className="pl-10 pr-4 py-2 w-full" />
           </div>
         </div>
 
@@ -56,48 +60,44 @@ export default function Navbar() {
         <div className="ml-auto">
           <ModeToggle />
         </div>
+      </div>
 
-        {/* Sidebar (for mobile) */}
-        {isMenuOpen && (
-          <div className="fixed inset-0 z-40 flex bg-black bg-opacity-50 md:hidden">
-            <aside className={`relative w-64 h-full bg-white transition-all duration-300 ${isMenuOpen ? 'translate-x-0' : 'translate-x-full'}`}>
-              {/* Close icon */}
-              <button
-                className="absolute top-0 right-0 p-4"
-                onClick={() => setIsMenuOpen(false)}
-              >
-                <XIcon className="h-6 w-6" />
-              </button>
+      {/* Sidebar (for mobile) */}
+      {isMenuOpen && (
+        <div className="fixed inset-0 z-40 flex bg-black bg-opacity-50 md:hidden">
+          <aside className={`relative w-64 h-full bg-white transition-all duration-300 ${isMenuOpen ? 'translate-x-0' : 'translate-x-full'}`}>
+            {/* Close icon */}
+            <button className="absolute top-0 right-0 p-4" onClick={() => setIsMenuOpen(false)}>
+              <XIcon className="h-6 w-6" />
+            </button>
 
-              {/* Sidebar content */}
-              <nav className="flex flex-col p-4"> 
-                <Link href="/" className="py-3">
-                  <span className="py-2 text-gray-800 dark:text-gray-300 cursor-pointer">Latest Stories</span>
-                </Link>
-                <Link href="/" className="py-3">
-                  <span className="py-2 text-gray-800 dark:text-gray-300 cursor-pointer">Product Reviews</span>
-                </Link>
-                <Link href="/" className="py-3">
-                  <span className="py-2 text-gray-800 dark:text-gray-300 cursor-pointer">AI Stories</span>
-                </Link>
-                <Link href="/" className="py-3">
-                  <span className="py-2 text-gray-800 dark:text-gray-300 cursor-pointer">Learn Programming</span>
-                </Link>
-              </nav>
+            {/* Sidebar content */}
+            <nav className="flex flex-col p-4">
+              {/* Navigation Links */}
+              <Link href="/" className="py-3">
+              <span className="py-2 text-gray-800 dark:text-gray-300 cursor-pointer">Latest Stories</span>
+              </Link>
+              <Link href="/" className="py-3">
+              <span className="py-2 text-gray-800 dark:text-gray-300 cursor-pointer">Product Reviews</span>
+              </Link>
+              <Link href="/" className="py-3">
+              <span className="py-2 text-gray-800 dark:text-gray-300 cursor-pointer">AI Stories</span>
+              </Link>
+              <Link href="/" className="py-3">
+              <span className="py-2 text-gray-800 dark:text-gray-300 cursor-pointer">Learn Programming</span>
+              </Link>
 
               {/* Mobile Search Bar */}
               <div className="mt-8 ml-3 mr-5"> 
                 <div className="relative">
                   <MagnifyingGlassIcon className="absolute left-3 top-1/2 transform -translate-y-1/2 h-5 w-5 text-gray-400" />
-                  <Input placeholder="Find your byte..." className="pl-10 pr-4 py-2 w-full" />
+                  <Input placeholder="Find an article..." className="pl-10 pr-4 py-2 w-full" />
                 </div>
               </div>
-
-            </aside>
-          </div>
-        )}
-      </nav>
-    </>
+            </nav>
+          </aside>
+        </div>
+      )}
+    </nav>
   );
 }
-
